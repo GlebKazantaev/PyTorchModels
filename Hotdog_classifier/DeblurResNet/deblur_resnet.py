4e352ef1e6bad3f7b3309af55e5230a123fb2a19
Gleb Kazantaev <gleb.kazantaev@intel.com>
2018-08-22 14:01:04 +0300

    Changed default weights distribution

--- Hotdog_classifier/DeblurResNet/deblur_resnet.py
+++ Hotdog_classifier/DeblurResNet/deblur_resnet.py
@@ -64,8 +64,7 @@ class DeblurResNet(nn.Module):
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                #nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                nn.init.constant_(m.weight, 1)
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
